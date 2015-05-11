7b693351b4e00a892b2091f2492b9eab920f542e
RomainSabathe <RSabathe@gmail.com>
2015-05-11 22:39:27 +0200

    [FR] Update in forms.py
    Two minor changes. At lines 220 and 251, the default value (if the submitting field is empty) is '' (empty string). The previous version resulted in a 'False' statement in the 'if'. Therefore, the value was changed, so that it returns a non-empty value (blank spaces). This therefore messed up with fields which were unrequired... It forced the cleaning of values we did not even write!

--- localflavor/fr/forms.py
+++ localflavor/fr/forms.py
@@ -217,7 +217,7 @@ class FRSIRENField(FRSIRENENumberMixin, CharField):
     }
 
     def prepare_value(self, value):
-        if value is None:
+        if not value:
             return value
         value = value.replace(' ', '').replace('-', '')
         return ' '.join((value[:3], value[3:6], value[6:]))
@@ -248,7 +248,7 @@ class FRSIRETField(FRSIRENENumberMixin, CharField):
         return ret
 
     def prepare_value(self, value):
-        if value is None:
+        if not value:
             return value
         value = value.replace(' ', '').replace('-', '')
         return ' '.join((value[:3], value[3:6], value[6:9], value[9:]))
